0ef376641b30d9672c0aba3fc10872af61411a38
Nana Nevase <nananevase9766@gmail.com>
2022-06-24 16:00:44 +0530

    web template pushed

---
+++ src/app/gallery/gallery-page/gallery-page.component.ts
@@ -0,0 +1,27 @@
+import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ConfigService } from "../../shared/services/config.service";
+
+@Component({
+  selector: "app-gallery-page",
+  templateUrl: "./gallery-page.component.html",
+  styleUrls: ["./gallery-page.component.css"],
+})
+export class GalleryPageComponent implements OnInit {
+  images$: Observable<any> = new Observable();
+
+  // images: {
+  //   id: number;
+  //   name: string;
+  // }[];
+
+  constructor(private config: ConfigService) {}
+
+  ngOnInit() {
+    this.getBlockData("images");
+  }
+
+  getBlockData(database: string) {
+    this.images$ = this.config.getSettings(database);
+  }
+}
